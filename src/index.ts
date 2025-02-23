56e5ec4aca3dd4f838a0e1d3a1dd9c8e3a3913b5
Mustafa Yelmer <mustafayelmer@gmail.com>
2025-02-24 01:40:01 +0300

    default generics export in index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {CacheBuilder, CacheChannel, CacheSet as CacheSetCommands, CacheHash as CacheHashCommands, CacheBasic as CacheBasicCommands, CachePropCompleted as CacheProp} from "@leyyo/cache";
+import {CacheBuilder, CacheChannel, CacheSet as CacheSetCommands, CacheHash as CacheHashCommands, CacheBasic as CacheBasicCommands, CachePropCompleted as CacheProp, CacheChannelDef, CacheBasicDef, CacheHashDef, CacheSetDef} from "@leyyo/cache";
 
 export * from './redis-channel';
-export {CacheBuilder, CacheChannel, CacheSetCommands, CacheHashCommands, CacheBasicCommands, CacheProp};
+export {CacheBuilder, CacheChannel, CacheSetCommands, CacheHashCommands, CacheBasicCommands, CacheProp, CacheChannelDef, CacheBasicDef, CacheHashDef, CacheSetDef};
