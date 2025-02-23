cad1809ed2d990ddfcd94b28e09fe7bb96d83603
Mustafa Yelmer <mustafayelmer@gmail.com>
2025-02-24 00:06:08 +0300

    extra export in index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {CacheBuilder} from "@leyyo/cache";
+import {CacheBuilder, CacheChannel, CacheSet as CacheSetCommands, CacheHash as CacheHashCommands, CacheBasic as CacheBasicCommands, CachePropCompleted as CacheProp} from "@leyyo/cache";
 
 export * from './redis-channel';
-export {CacheBuilder};
+export {CacheBuilder, CacheChannel, CacheSetCommands, CacheHashCommands, CacheBasicCommands, CacheProp};
